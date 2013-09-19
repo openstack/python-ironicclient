95d86630b1b16152f3d71808e67f61c74de73298
Lucas Alvares Gomes <lucasagomes@gmail.com>
2013-09-19 17:42:35 +0100

    Add basic tests
    Change-Id: I058cb2a91399fb7185f1b23aefe441892f235dcc

---
+++ ironicclient/tests/test_shell.py
@@ -0,0 +1,101 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import cStringIO
+import httplib2
+import re
+import sys
+
+import fixtures
+from testtools import matchers
+
+from keystoneclient.v2_0 import client as ksclient
+
+from ironicclient import exc
+from ironicclient import shell as ironic_shell
+from ironicclient.tests import utils
+from ironicclient.v1 import client as v1client
+
+FAKE_ENV = {'OS_USERNAME': 'username',
+            'OS_PASSWORD': 'password',
+            'OS_TENANT_NAME': 'tenant_name',
+            'OS_AUTH_URL': 'http://no.where'}
+
+
+class ShellTest(utils.BaseTestCase):
+    re_options = re.DOTALL | re.MULTILINE
+
+    # Patch os.environ to avoid required auth info.
+    def make_env(self, exclude=None):
+        env = dict((k, v) for k, v in FAKE_ENV.items() if k != exclude)
+        self.useFixture(fixtures.MonkeyPatch('os.environ', env))
+
+    def setUp(self):
+        super(ShellTest, self).setUp()
+        self.m.StubOutWithMock(ksclient, 'Client')
+        self.m.StubOutWithMock(v1client.Client, 'json_request')
+        self.m.StubOutWithMock(v1client.Client, 'raw_request')
+
+    def shell(self, argstr):
+        orig = sys.stdout
+        try:
+            sys.stdout = cStringIO.StringIO()
+            _shell = ironic_shell.IronicShell()
+            _shell.main(argstr.split())
+        except SystemExit:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            self.assertEqual(exc_value.code, 0)
+        finally:
+            out = sys.stdout.getvalue()
+            sys.stdout.close()
+            sys.stdout = orig
+
+        return out
+
+    def test_help_unknown_command(self):
+        self.assertRaises(exc.CommandError, self.shell, 'help foofoo')
+
+    def test_debug(self):
+        httplib2.debuglevel = 0
+        self.shell('--debug help')
+        self.assertEqual(httplib2.debuglevel, 1)
+
+    def test_help(self):
+        required = [
+            '.*?^usage: ironic',
+            '.*?^See "ironic help COMMAND" '
+            'for help on a specific command',
+        ]
+        for argstr in ['--help', 'help']:
+            help_text = self.shell(argstr)
+            for r in required:
+                self.assertThat(help_text,
+                                matchers.MatchesRegex(r,
+                                                      self.re_options))
+
+    def test_help_on_subcommand(self):
+        required = [
+            '.*?^usage: ironic chassis-show',
+            ".*?^Show a chassis",
+        ]
+        argstrings = [
+            'help chassis-show',
+        ]
+        for argstr in argstrings:
+            help_text = self.shell(argstr)
+            for r in required:
+                self.assertThat(help_text,
+                                matchers.MatchesRegex(r, self.re_options))
+
+    def test_auth_param(self):
+        self.make_env(exclude='OS_USERNAME')
+        self.test_help()
---
+++ ironicclient/tests/test_utils.py
@@ -0,0 +1,59 @@
+# Copyright 2013 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import cStringIO
+import sys
+
+from ironicclient.common import utils
+from ironicclient.tests import utils as test_utils
+
+
+class UtilsTest(test_utils.BaseTestCase):
+
+    def test_prettytable(self):
+        class Struct:
+            def __init__(self, **entries):
+                self.__dict__.update(entries)
+
+        # test that the prettytable output is wellformatted (left-aligned)
+        saved_stdout = sys.stdout
+        try:
+            sys.stdout = output_dict = cStringIO.StringIO()
+            utils.print_dict({'K': 'k', 'Key': 'Value'})
+
+        finally:
+            sys.stdout = saved_stdout
+
+        self.assertEqual(output_dict.getvalue(), '''\
++----------+-------+
+| Property | Value |
++----------+-------+
+| K        | k     |
+| Key      | Value |
++----------+-------+
+''')
+
+    def test_args_array_to_dict(self):
+        my_args = {
+            'matching_metadata': ['metadata.key=metadata_value'],
+            'other': 'value'
+        }
+        cleaned_dict = utils.args_array_to_dict(my_args,
+                                                "matching_metadata")
+        self.assertEqual(cleaned_dict, {
+            'matching_metadata': {'metadata.key': 'metadata_value'},
+            'other': 'value'
+        })
