6752ae83072c99d4cc44ff0b8155e34c4dc42cac
Chris Krelle <nobodycam@gmail.com>
2013-09-27 09:10:21 -0700

    Change service_type to baremetal
    update default service_type to baremetal.
    
    Change-Id: I58856885268225b79218d7312472d05b0a47b5b7

--- ironicclient/client.py
+++ ironicclient/client.py
@@ -35,7 +35,7 @@ def _get_ksclient(**kwargs):
 def _get_endpoint(client, **kwargs):
     """Get an endpoint using the provided keystone client."""
     return client.service_catalog.url_for(
-        service_type=kwargs.get('service_type') or 'ironic',
+        service_type=kwargs.get('service_type') or 'baremetal',
         endpoint_type=kwargs.get('endpoint_type') or 'publicURL')
 
 
