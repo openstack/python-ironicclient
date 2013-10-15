ebb6b304e890e1855739828c57234f8f24215225
Kui Shi <skuicloud@gmail.com>
2013-10-16 05:34:37 +0800

    Replace basestring with six.string_types
    Partial implement: blueprint py33-support
    
    Change-Id: Ibf1c63d407698f37b333da8f90bbdedbd9512c75

--- ironicclient/common/utils.py
+++ ironicclient/common/utils.py
@@ -112,7 +112,7 @@ def print_dict(d, dict_property="Property", wrap=0):
             v = textwrap.fill(str(v), wrap)
         # if value has a newline, add in multiple rows
         # e.g. fault with stacktrace
-        if v and isinstance(v, basestring) and r'\n' in v:
+        if v and isinstance(v, six.string_types) and r'\n' in v:
             lines = v.strip().split(r'\n')
             col1 = k
             for line in lines:
