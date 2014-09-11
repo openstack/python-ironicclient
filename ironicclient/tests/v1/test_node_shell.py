58b0b12db2139c6eba5efac5caf9b9dd18170de6
Yuiko Takada <takada-yuiko@mxn.nes.nec.co.jp>
2014-09-11 16:18:38 +0900

    Add unit tests for "ironic node-delete" shell cmd
    This patch set add unit tests for "ironic node-delete" command.
    
    Change-Id: I41b3e443f3777347a9353bde19e8968668b6c40b

--- ironicclient/tests/v1/test_node_shell.py
+++ ironicclient/tests/v1/test_node_shell.py
@@ -49,6 +49,23 @@ class NodeShellTest(utils.BaseTestCase):
         act = actual.keys()
         self.assertEqual(sorted(exp), sorted(act))
 
+    def test_do_node_delete(self):
+        client_mock = mock.MagicMock()
+        args = mock.MagicMock()
+        args.node = ['node_uuid']
+
+        n_shell.do_node_delete(client_mock, args)
+        client_mock.node.delete.assert_called_once_with('node_uuid')
+
+    def test_do_node_delete_multiple(self):
+        client_mock = mock.MagicMock()
+        args = mock.MagicMock()
+        args.node = ['node_uuid1', 'node_uuid2']
+
+        n_shell.do_node_delete(client_mock, args)
+        client_mock.node.delete.assert_has_calls(
+            [mock.call('node_uuid1'), mock.call('node_uuid2')])
+
     def test_do_node_vendor_passthru_with_args(self):
         client_mock = mock.MagicMock()
         args = mock.MagicMock()
