8224859c02f525fa3b7dbe917d56b889b0dbb073
Dmitry Tantsur <dtantsur@redhat.com>
2015-12-11 12:38:39 +0100

    Scale back on how many warnings we issue
    * Remove "Request returned failure status" warning, it does not carry
      any helpful information for an operator (or anyone).
    * Demote retry warnings to debug level. Retrying is essentially a normal
      thing in the real world, and currently we're spamming logs with warnings.
      http://logs.openstack.org/93/255793/3/gate/gate-tempest-dsvm-ironic-agent_ssh/25175ed/logs/screen-n-cpu.txt.gz?level=TRACE
    
    Change-Id: I6c40581b9dc5ab1fe3af4b882ede17a13edacc66
    Closes-Bug: #1525002

--- ironicclient/common/http.py
+++ ironicclient/common/http.py
@@ -189,7 +189,7 @@ def with_retries(func):
                     LOG.error(msg)
                     raise
                 else:
-                    LOG.warning(msg)
+                    LOG.debug(msg)
                     time.sleep(self.conflict_retry_interval)
 
     return wrapper
@@ -348,7 +348,6 @@ class HTTPClient(VersionNegotiationMixin):
             self.log_http_response(resp)
 
         if 400 <= resp.status < 600:
-            LOG.warning(_LW("Request returned failure status"))
             error_json = _extract_error_json(body_str)
             raise exc.from_response(
                 resp, error_json.get('faultstring'),
