deae37ae26eab40ba66cc0fcf2f4d62cc75df22d
Kyrylo Romanenko <kromanenko@mirantis.com>
2016-04-25 19:22:17 +0300

    Fix typo in docstring of assertTableHeaders method
    Change-Id: I79d5204a9749d55fa1229417a2b04735d1520456

--- ironicclient/tests/functional/base.py
+++ ironicclient/tests/functional/base.py
@@ -167,7 +167,7 @@ class FunctionalTestBase(base.ClientTestBase):
         """Assert that field_names and table_headers are equal.
 
         :param field_names: field names from the output table of the cmd
-        :param table_heades: table headers output from cmd
+        :param table_headers: table headers output from cmd
         """
         self.assertEqual(sorted(field_names), sorted(table_headers))
 
