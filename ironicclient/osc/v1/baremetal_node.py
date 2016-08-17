fd54fd076f13565850546daf4c209a0c6994a782
Ruby Loo <ruby.loo@intel.com>
2016-08-17 17:50:19 -0400

    Adds 'openstack baremetal node console' commands
    Adds the following console-related commands to the
    OpenStackClient plugin:
        openstack baremetal node console disable
        openstack baremetal node console enable
        openstack baremetal node console show
    
    Change-Id: I060531baf84907cdf3763369a43469ec1c860e4d
    Partial-Bug: #1526479

--- ironicclient/osc/v1/baremetal_node.py
+++ ironicclient/osc/v1/baremetal_node.py
@@ -110,6 +110,73 @@ class CleanBaremetalNode(ProvisionStateBaremetalNode):
             cleansteps=clean_steps)
 
 
+class ConsoleDisableBaremetalNode(command.Command):
+    """Disable console access for a node"""
+
+    log = logging.getLogger(__name__ + ".ConsoleDisableBaremetalNode")
+
+    def get_parser(self, prog_name):
+        parser = super(ConsoleDisableBaremetalNode, self).get_parser(prog_name)
+
+        parser.add_argument(
+            'node',
+            metavar='<node>',
+            help="Name or UUID of the node"
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        baremetal_client = self.app.client_manager.baremetal
+        baremetal_client.node.set_console_mode(parsed_args.node, False)
+
+
+class ConsoleEnableBaremetalNode(command.Command):
+    """Enable console access for a node"""
+
+    log = logging.getLogger(__name__ + ".ConsoleEnableBaremetalNode")
+
+    def get_parser(self, prog_name):
+        parser = super(ConsoleEnableBaremetalNode, self).get_parser(prog_name)
+
+        parser.add_argument(
+            'node',
+            metavar='<node>',
+            help="Name or UUID of the node"
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        baremetal_client = self.app.client_manager.baremetal
+        baremetal_client.node.set_console_mode(parsed_args.node, True)
+
+
+class ConsoleShowBaremetalNode(command.ShowOne):
+    """Show console information for a node"""
+
+    log = logging.getLogger(__name__ + ".ConsoleShowBaremetalNode")
+
+    def get_parser(self, prog_name):
+        parser = super(ConsoleShowBaremetalNode, self).get_parser(prog_name)
+
+        parser.add_argument(
+            'node',
+            metavar='<node>',
+            help="Name or UUID of the node"
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        baremetal_client = self.app.client_manager.baremetal
+        info = baremetal_client.node.get_console(parsed_args.node)
+        return zip(*sorted(info.items()))
+
+
 class CreateBaremetalNode(command.ShowOne):
     """Register a new node with the baremetal service"""
 
