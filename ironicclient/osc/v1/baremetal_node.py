d8cd1ce54bdd27146cdf9918b06abf52a9b57bd9
Ruby Loo <ruby.loo@intel.com>
2016-08-23 12:15:18 -0400

    Add node validate OSC command
    Adds 'openstack baremetal node validate' command to the OpenStackClient
    plugin.
    
    Change-Id: I53c0f949f2c0f2c353c40b3333669ed64c1062c4
    Partial-Bug: #1526479

--- ironicclient/osc/v1/baremetal_node.py
+++ ironicclient/osc/v1/baremetal_node.py
@@ -1103,3 +1103,36 @@ class UnsetBaremetal(UnsetBaremetalNode):
         self.log.warning("This command is deprecated. Instead, use "
                          "'openstack baremetal node unset'.")
         super(UnsetBaremetal, self).take_action(parsed_args)
+
+
+class ValidateBaremetalNode(command.Lister):
+    """Validate a node's driver interfaces"""
+
+    log = logging.getLogger(__name__ + ".ValidateBaremetalNode")
+
+    def get_parser(self, prog_name):
+        parser = super(ValidateBaremetalNode, self).get_parser(prog_name)
+
+        parser.add_argument(
+            'node',
+            metavar='<node>',
+            help="Name or UUID of the node")
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        baremetal_client = self.app.client_manager.baremetal
+
+        interfaces = baremetal_client.node.validate(parsed_args.node)._info
+        data = []
+        for key, value in interfaces.items():
+            interface = {'interface': key}
+            interface.update(value)
+            data.append(interface)
+        field_labels = ['Interface', 'Result', 'Reason']
+        fields = ['interface', 'result', 'reason']
+        data = oscutils.sort_items(data, 'interface')
+        return (field_labels,
+                (oscutils.get_dict_properties(s, fields) for s in data))
