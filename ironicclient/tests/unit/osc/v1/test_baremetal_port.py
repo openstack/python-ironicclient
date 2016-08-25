7513aae7e23fb76766afb69949a5a93503852e72
Rodion Promyshlennikov <rpromyshlennikov@mirantis.com>
2016-08-25 20:34:00 +0300

    Add baremetal port delete command to OSC plugin
    Change-Id: I4d08ebf38fda739840a1bb98c0bc5bf2ff769b65
    Partial-Bug: #1526479

--- ironicclient/tests/unit/osc/v1/test_baremetal_port.py
+++ ironicclient/tests/unit/osc/v1/test_baremetal_port.py
@@ -19,6 +19,7 @@ import copy
 import mock
 from osc_lib.tests import utils as oscutils
 
+from ironicclient import exc
 from ironicclient.osc.v1 import baremetal_port
 from ironicclient.tests.unit.osc.v1 import fakes as baremetal_fakes
 
@@ -292,6 +293,64 @@ class TestBaremetalPortSet(TestBaremetalPort):
     def test_baremetal_port_set_no_options(self):
         arglist = []
         verifylist = []
+        self.assertRaises(oscutils.ParserException,
+                          self.check_parser,
+                          self.cmd, arglist, verifylist)
+
+
+class TestBaremetalPortDelete(TestBaremetalPort):
+    def setUp(self):
+        super(TestBaremetalPortDelete, self).setUp()
+
+        self.baremetal_mock.port.get.return_value = (
+            baremetal_fakes.FakeBaremetalResource(
+                None,
+                copy.deepcopy(baremetal_fakes.BAREMETAL_PORT),
+                loaded=True))
+
+        self.cmd = baremetal_port.DeleteBaremetalPort(self.app, None)
+
+    def test_baremetal_port_delete(self):
+        arglist = ['zzz-zzzzzz-zzzz']
+        verifylist = []
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        self.cmd.take_action(parsed_args)
+
+        args = 'zzz-zzzzzz-zzzz'
+        self.baremetal_mock.port.delete.assert_called_with(args)
+
+    def test_baremetal_port_delete_multiple(self):
+        arglist = ['zzz-zzzzzz-zzzz', 'fakename']
+        verifylist = []
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        self.cmd.take_action(parsed_args)
+
+        args = ['zzz-zzzzzz-zzzz', 'fakename']
+        self.baremetal_mock.port.delete.has_calls(
+            [mock.call(x) for x in args])
+        self.assertEqual(2, self.baremetal_mock.port.delete.call_count)
+
+    def test_baremetal_port_delete_multiple_with_fail(self):
+        arglist = ['zzz-zzzzzz-zzzz', 'badname']
+        verifylist = []
+
+        self.baremetal_mock.port.delete.side_effect = ['', exc.ClientException]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        self.assertRaises(exc.ClientException,
+                          self.cmd.take_action,
+                          parsed_args)
+
+        args = ['zzz-zzzzzz-zzzz', 'badname']
+        self.baremetal_mock.port.delete.has_calls(
+            [mock.call(x) for x in args])
+        self.assertEqual(2, self.baremetal_mock.port.delete.call_count)
+
+    def test_baremetal_port_delete_no_port(self):
+        arglist = []
+        verifylist = []
+
         self.assertRaises(oscutils.ParserException,
                           self.check_parser,
                           self.cmd, arglist, verifylist)
