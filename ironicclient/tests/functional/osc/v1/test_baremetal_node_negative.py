540c59bf9c4909678deb7dc141c64a0f87bfbd0c
Rodion Promyshlennikov <rpromyshlennikov@mirantis.com>
2016-08-26 18:20:58 +0300

    Add negative tests for baremetal node commands
    Change-Id: I56e64cc37ffbce448dfef3c9dc34c909c308c7b2

---
+++ ironicclient/tests/functional/osc/v1/test_baremetal_node_negative.py
@@ -0,0 +1,89 @@
+#    Copyright (c) 2017 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import ddt
+import six
+from tempest.lib import exceptions
+
+from ironicclient.tests.functional.osc.v1 import base
+
+
+@ddt.ddt
+class BaremetalNodeNegativeTests(base.TestCase):
+    """Negative tests for baremetal node commands."""
+
+    def setUp(self):
+        super(BaremetalNodeNegativeTests, self).setUp()
+        self.node = self.node_create()
+
+    @staticmethod
+    def constuct_cmd(base_cmd, argument, value):
+        cmd = base_cmd
+        if argument:
+            cmd = '{} {} {}'.format(cmd, argument, value)
+        return cmd
+
+    @ddt.data(
+        ('', '', 'error: argument --driver is required'),
+        ('--driver', 'wrongdriver',
+         'No valid host was found. Reason: No conductor service '
+         'registered which supports driver wrongdriver.')
+    )
+    @ddt.unpack
+    def test_create_driver(self, argument, value, ex_text):
+        """Negative test for baremetal node driver options."""
+        base_cmd = 'baremetal node create'
+        command = self.constuct_cmd(base_cmd, argument, value)
+        six.assertRaisesRegex(self, exceptions.CommandFailed, ex_text,
+                              self.openstack, command)
+
+    def test_delete_no_node(self):
+        """Test for baremetal node delete without node specified."""
+        command = 'baremetal node delete'
+        ex_text = 'error: too few arguments'
+        six.assertRaisesRegex(self, exceptions.CommandFailed, ex_text,
+                              self.openstack, command)
+
+    def test_list_wrong_argument(self):
+        """Test for baremetal node list with wrong argument."""
+        command = 'baremetal node list --wrong_arg'
+        ex_text = 'error: unrecognized arguments: --wrong_arg'
+        six.assertRaisesRegex(self, exceptions.CommandFailed, ex_text,
+                              self.openstack, command)
+
+    @ddt.data(
+        ('--property', '', 'error: too few arguments'),
+        ('--property', 'prop', 'Attributes must be a list of PATH=VALUE')
+    )
+    @ddt.unpack
+    def test_set_property(self, argument, value, ex_text):
+        """Negative test for baremetal node set command options."""
+        base_cmd = 'baremetal node set'
+        command = '{} {}'.format(
+            self.constuct_cmd(base_cmd, argument, value), self.node['uuid'])
+        six.assertRaisesRegex(self, exceptions.CommandFailed, ex_text,
+                              self.openstack, command)
+
+    @ddt.data(
+        ('--property', '', 'error: too few arguments'),
+        ('--property', 'prop', "Reason: can't remove non-existent object")
+    )
+    @ddt.unpack
+    def test_unset_property(self, argument, value, ex_text):
+        """Negative test for baremetal node unset command options."""
+        base_cmd = 'baremetal node unset'
+        command = '{} {}'.format(
+            self.constuct_cmd(base_cmd, argument, value), self.node['uuid'])
+        six.assertRaisesRegex(self, exceptions.CommandFailed, ex_text,
+                              self.openstack, command)
