11eae85a318d3341f0c114f0de1aa5032baf95d2
Kyrylo Romanenko <kromanenko@mirantis.com>
2016-09-28 13:53:14 +0300

    Fix import of ironicclient and reformat docstring
    Fix import of ironicclient to ironicclient/shell.py.
    Reformat docstring in ironicclient/v1/shell.py.
    
    Change-Id: Iad717d139ef423bb9f93050c6ba0c20fe1912a82

--- ironicclient/v1/shell.py
+++ ironicclient/v1/shell.py
@@ -33,8 +33,9 @@ def enhance_parser(parser, subparsers, cmd_mapper):
     Take a basic (nonversioned) parser and enhance it with
     commands and options specific for this version of API.
 
-    :param parser: top level parser :param subparsers: top level
-        parser's subparsers collection where subcommands will go
+    :param parser: top level parser
+    :param subparsers: top level parser's subparsers collection
+                       where subcommands will go
     """
     for command_module in COMMAND_MODULES:
         utils.define_commands_from_module(subparsers, command_module,
