c27c848dba205e163a0828c8caeb4cb819c2d23c
Vladyslav Drok <vdrok@mirantis.com>
2020-01-28 17:19:55 +0100

    Remove VerifiedHTTPSConnection class
    It was needed for now EOL python versions, was unused for quite
    a while and is insecure (does not allow customising TLS version
    for example).
    
    Change-Id: Ia382d3fc155196e7cf916f57ee0ac6be7dde07af

--- ironicclient/common/http.py
+++ ironicclient/common/http.py
@@ -17,10 +17,7 @@ from distutils.version import StrictVersion
 import functools
 from http import client as http_client
 import logging
-import os
 import re
-import socket
-import ssl
 import textwrap
 import time
 from urllib import parse as urlparse
@@ -301,70 +298,6 @@ def with_retries(func):
                     time.sleep(self.conflict_retry_interval)
 
     return wrapper
-
-
-class VerifiedHTTPSConnection(http_client.HTTPSConnection):
-    """httplib-compatible connection using client-side SSL authentication
-
-    :see http://code.activestate.com/recipes/
-            577548-https-httplib-client-connection-with-certificate-v/
-    """
-
-    def __init__(self, host, port, key_file=None, cert_file=None,
-                 ca_file=None, timeout=None, insecure=False):
-        http_client.HTTPSConnection.__init__(self, host, port,
-                                             key_file=key_file,
-                                             cert_file=cert_file)
-        self.key_file = key_file
-        self.cert_file = cert_file
-        if ca_file is not None:
-            self.ca_file = ca_file
-        else:
-            self.ca_file = self.get_system_ca_file()
-        self.timeout = timeout
-        self.insecure = insecure
-
-    def connect(self):
-        """Connect to a host on a given (SSL) port.
-
-        If ca_file is pointing somewhere, use it to check Server Certificate.
-
-        Redefined/copied and extended from httplib.py:1105 (Python 2.6.x).
-        This is needed to pass cert_reqs=ssl.CERT_REQUIRED as parameter to
-        ssl.wrap_socket(), which forces SSL to check server certificate against
-        our client certificate.
-        """
-        sock = socket.create_connection((self.host, self.port), self.timeout)
-
-        if self._tunnel_host:
-            self.sock = sock
-            self._tunnel()
-
-        if self.insecure is True:
-            kwargs = {'cert_reqs': ssl.CERT_NONE}
-        else:
-            kwargs = {'cert_reqs': ssl.CERT_REQUIRED, 'ca_certs': self.ca_file}
-
-        if self.cert_file:
-            kwargs['certfile'] = self.cert_file
-            if self.key_file:
-                kwargs['keyfile'] = self.key_file
-
-        self.sock = ssl.wrap_socket(sock, **kwargs)
-
-    @staticmethod
-    def get_system_ca_file():
-        """Return path to system default CA file."""
-        # Standard CA file locations for Debian/Ubuntu, RedHat/Fedora,
-        # Suse, FreeBSD/OpenBSD
-        ca_path = ['/etc/ssl/certs/ca-certificates.crt',
-                   '/etc/pki/tls/certs/ca-bundle.crt',
-                   '/etc/ssl/ca-bundle.pem',
-                   '/etc/ssl/cert.pem']
-        for ca in ca_path:
-            if os.path.exists(ca):
-                return ca
-        return None
 
 
 class SessionClient(VersionNegotiationMixin, adapter.LegacyJsonAdapter):
