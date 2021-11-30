2e0cef2cb0b1297788dc187fc61816408060c616
Julia Kreger <juliaashleykreger@gmail.com>
2021-11-30 15:10:43 -0800

    Add node history support
    Adds support for rest API version 1.78, which
    covers the node history feature.
    
    Story: 2002980
    Task: 43319
    Change-Id: I6edbc38353a4b2f7b0a758108bc91cc9fb72a29d

--- ironicclient/tests/unit/osc/v1/test_baremetal_node.py
+++ ironicclient/tests/unit/osc/v1/test_baremetal_node.py
@@ -4188,3 +4188,59 @@ class TestBIOSSettingShow(TestBaremetal):
             'node_uuid', 'bios_name_1')
         expected_data = ('bios_name_1', 'bios_value_1')
         self.assertEqual(expected_data, tuple(data))
+
+
+class TestNodeHistoryEventList(TestBaremetal):
+    def setUp(self):
+        super(TestNodeHistoryEventList, self).setUp()
+
+        self.baremetal_mock.node.get_history_list.return_value = (
+            baremetal_fakes.NODE_HISTORY)
+
+        # Get the command object to test
+        self.cmd = baremetal_node.NodeHistoryList(self.app, None)
+
+    def test_baremetal_node_history_list(self):
+        arglist = ['node_uuid', '--long']
+        verifylist = [('node', 'node_uuid'), ('long', True)]
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        columns, data = self.cmd.take_action(parsed_args)
+        self.baremetal_mock.node.get_history_list.assert_called_once_with(
+            'node_uuid', True)
+        expected_columns = ('UUID', 'Created At', 'Severity',
+                            'Event Origin Type', 'Description of the event',
+                            'Conductor', 'User')
+        expected_data = (('abcdef1', 'time', 'info', 'purring', 'meow',
+                          'lap-conductor', '0191'),)
+        self.assertEqual(expected_columns, columns)
+        self.assertEqual(expected_data, tuple(data))
+
+
+class TestNodeHistoryEventGet(TestBaremetal):
+    def setUp(self):
+        super(TestNodeHistoryEventGet, self).setUp()
+
+        self.baremetal_mock.node.get_history_event.return_value = (
+            baremetal_fakes.NODE_HISTORY[0])
+
+        # Get the command object to test
+        self.cmd = baremetal_node.NodeHistoryEventGet(self.app, None)
+
+    def test_baremetal_node_history_list(self):
+        arglist = ['node_uuid', 'event_uuid']
+        verifylist = [('node', 'node_uuid'), ('event', 'event_uuid')]
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        columns, data = self.cmd.take_action(parsed_args)
+        self.baremetal_mock.node.get_history_event.assert_called_once_with(
+            'node_uuid', 'event_uuid')
+        expected_columns = ('conductor', 'created_at', 'event', 'event_type',
+                            'severity', 'user', 'uuid')
+        expected_data = ('lap-conductor', 'time', 'meow', 'purring', 'info',
+                         '0191', 'abcdef1')
+
+        self.assertEqual(expected_columns, columns)
+        self.assertEqual(expected_data, tuple(data))
